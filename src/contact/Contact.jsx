820b81e43c31cf9adea5a89976a82246cd36ca61
John Madden <jck13mad@gmail.com>
2021-10-14 10:58:28 -0500

    Change link to linkedin

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -111,8 +111,8 @@ class Contact extends Component {
                     <div className="bottom">
                         <ul>
                             <li><a href="https://twitter.com/jackathon3" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                            <li><a href="mailto:jck13mad@gmail.com" target="_blank" rel="noopener noreferrer">Email</a></li>
-                            <li><a href="https://www.linkedin.com/in/john-madden-063078188/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                            <li><a href="mailto:jck13mad@gmail.com" target="_blank" rel="noopener noreferrer" alt="jck13mad@gmail.com">Email</a></li>
+                            <li><a href="https://www.linkedin.com/in/jackcharlesmadden/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
                             <li><a href="https://github.com/jck13mad?tab=repositories" target="_blank" rel="noopener noreferrer">Github</a></li>
                         </ul>
                     </div>
